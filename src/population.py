e3d404b808652e588e7a89898383f0d8d74d0b0a
Seongoh Ryoo <seongohr@usc.edu>
2020-11-11 18:31:15 -0800

    add exception case in population

--- src/population.py
+++ src/population.py
@@ -47,13 +47,26 @@ def make_data(heading, contents):
         code = row[CBSA_code]
         if code == '':
             continue
-        not_num_ct = 0
+        not_num_ct = 0  # used for denominator used for calculating avg population change
         title = row[CBSA_title]
         ct = row[census_tracts]
-        p00 = int(row[pop00])
-        p10 = int(row[pop10])
+        p00 = row[pop00]
+        p10 = row[pop10]
         p_ch = (row[ppchg].replace(',', ''))
 
+        # check population in 2000 can be converted to integer
+        try:
+            p00 = int(row[pop00])
+        except:
+            p00 = 0
+
+        # check population in 2010 can be converted to integer
+        try:
+            p10 = int(row[pop10])
+        except:
+            p10 = 0
+
+        # check population change can be converted to float
         try:
             p_ch = float(p_ch)
         except:
@@ -70,12 +83,13 @@ def make_data(heading, contents):
                                'not_num_tract10': not_num_ct
                                }
         else:
-            CBSA_dict[code]['title'] = title
+            if CBSA_dict[code]['title'] == '':
+                CBSA_dict[code]['title'] = title
             if ct not in CBSA_dict[code]['tract10']:
                 CBSA_dict[code]['tract10'].add(ct)
                 CBSA_dict[code]['num_tract10'] += 1
-            CBSA_dict[code]['pop00'] += int(p00)
-            CBSA_dict[code]['pop10'] += int(p10)
+            CBSA_dict[code]['pop00'] += p00
+            CBSA_dict[code]['pop10'] += p10
             CBSA_dict[code]['ppchg'] += p_ch
             CBSA_dict[code]['not_num_tract10'] += not_num_ct
 
